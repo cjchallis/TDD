aca58be0a9bbf53d49552ec3c3064a1ee1dcbb0c
cjchallis <christopher.challis@gmail.com>
2016-11-03 02:21:54 +0000

    New URL + view for adding to lists, FT passes

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -88,5 +88,4 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Buy milk', page_text)
     
         # Satisfied, they both go back to sleep
-        self.fail('Finish the test!')
 
